6dcfbe9914a5b4f9fd246bfa36f730738d9d7010
Yannik Selbach <yannik.selbach@smail.th-koeln.de>
2021-09-15 12:04:33 +0200

    setup frontend vue

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div>
+            <v-btn
+              color="green" fab large dark>
+              <v-icon>mdi-alarm</v-icon>
+            </v-btn>
+          </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
